3bbfbc9bf76a6222ee315416284442cd3a66514c
ajmalmirsha <ajmalmirshaa2004@gmail.com>
2024-03-20 10:55:12 +0530

    body-parser removed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require("express")
 
 const session = require('express-session')
 
-const bodyParser=require('body-parser')
+// const bodyParser=require('body-parser')
 
 
 const app= express()
